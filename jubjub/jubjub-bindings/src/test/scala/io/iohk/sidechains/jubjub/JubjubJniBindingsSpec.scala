d87f4723e8a689b6e8075991d9a2ca0adc7d174b
Lech Głowiak <lech.glowiak@iohk.io>
2023-03-22 16:39:51 +0100

    Add PrivateKey and PublicKey 'types'

--- jubjub/jubjub-bindings/src/test/scala/io/iohk/sidechains/jubjub/JubjubJniBindingsSpec.scala
+++ jubjub/jubjub-bindings/src/test/scala/io/iohk/sidechains/jubjub/JubjubJniBindingsSpec.scala
@@ -1,9 +1,9 @@
 package io.iohk.sidechains.jubjub
 
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpec
 import io.iohk.sidechains.Hex
 import org.scalatest.EitherValues
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
 import scala.util.Try
 
@@ -11,13 +11,17 @@ class JubjubJniBindingsSpec extends AnyWordSpec with Matchers with EitherValues 
 
   val bindings = new JubjubJniBindings
 
-  val message: Array[Byte] = Hex.decodeUnsafe("01234567890abcdef")
+  val message: Array[Byte]  = Hex.decodeUnsafe("01234567890abcdef")
   val message2: Array[Byte] = Hex.decodeUnsafe("fedc456789000")
 
-  val privateKey: Array[Byte] = Hex.decodeUnsafe("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f")
+  val privateKey: Array[Byte] = Hex.decodeUnsafe(
+    "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f"
+  )
   val publicKey: Array[Byte] = Hex.decodeUnsafe("f94c240b26cdb868cf6d56caa620612702d0294ae044acade84250d3eab653df")
 
-  val privateKey2: Array[Byte] = Hex.decodeUnsafe("fff1f2f3f4f5f6f7f8f9fafbfcfdfeff1f1112131415161718191a1b1c1d1e1f2f2122232425262728292a2b2c2d2e2f3f3132333435363738393a3b3c3d3e3f")
+  val privateKey2: Array[Byte] = Hex.decodeUnsafe(
+    "fff1f2f3f4f5f6f7f8f9fafbfcfdfeff1f1112131415161718191a1b1c1d1e1f2f2122232425262728292a2b2c2d2e2f3f3132333435363738393a3b3c3d3e3f"
+  )
   val publicKey2: Array[Byte] = Hex.decodeUnsafe("e8a335e943810f349af4b57fc87644367dea8dc1b6c6d804e3bf36d966d574ec")
 
   "JubJubJniBindings" should {
@@ -32,26 +36,26 @@ class JubjubJniBindingsSpec extends AnyWordSpec with Matchers with EitherValues 
     }
 
     "pass of sign-verify round trip" in {
-      val signature = bindings.sign(message, privateKey)
+      val signature          = bindings.sign(message, privateKey)
       val verificationResult = bindings.verify(message, signature, publicKey);
       assert(verificationResult)
     }
 
     "fail verify if public key is invalid" in {
       val signature = bindings.sign(message, privateKey)
-      val errMsg = Try(bindings.verify(message, signature, Array.fill(32)(0.toByte))).toEither.swap.map(_.getMessage)
+      val errMsg    = Try(bindings.verify(message, signature, Array.fill(32)(0.toByte))).toEither.swap.map(_.getMessage)
       assert(errMsg.value == "public key is invalid")
     }
 
     "fail not verify if public key doesn't match" in {
       val signature = bindings.sign(message, privateKey)
-      val result = bindings.verify(message, signature, publicKey2)
+      val result    = bindings.verify(message, signature, publicKey2)
       assert(!result)
     }
 
     "not verify if message doesn't match" in {
       val signature = bindings.sign(message, privateKey)
-      val result = bindings.verify(message2, signature, publicKey)
+      val result    = bindings.verify(message2, signature, publicKey)
       assert(!result)
     }
 
--- jubjub/jubjub-native/src/native/src/lib.rs
+++ jubjub/jubjub-native/src/native/src/lib.rs
@@ -1,16 +1,13 @@
+use eddsa::{sign, verify, EdDsaSignature};
+use group::{Group, GroupEncoding};
 use jni::objects::JObject;
 use jni::sys::{jbyteArray, jobjectArray};
 use jni::JNIEnv;
-
-use crate::conversions::{
-    byte_array_from_java, byte_array_to_java, java_byte_array_to_jubjub_eddsa_sig,
-    java_byte_array_to_jubjub_private_key, java_byte_array_to_jubjub_public_key, return_or_throw,
-};
-use eddsa::{sign, verify};
-use group::{Group, GroupEncoding};
 use jubjub::{Scalar, SubgroupPoint};
+use std::convert::TryInto;
 
-mod conversions;
+type PrivateKey = Scalar;
+type PublicKey = SubgroupPoint;
 
 #[no_mangle]
 pub extern "system" fn Java_io_iohk_sidechains_jubjub_JubjubJniBindings_derivePublicKey(
@@ -37,7 +34,7 @@ pub extern "system" fn Java_io_iohk_sidechains_jubjub_JubjubJniBindings_sign(
     let result = byte_array_from_java(env, msg)
         .and_then(|msg| {
             java_byte_array_to_jubjub_private_key(env, prv_key).map(|prv_key| {
-                let signature: (SubgroupPoint, Scalar) = sign(&msg, prv_key);
+                let signature: EdDsaSignature = sign(&msg, prv_key);
                 let mut r_bytes = signature.0.to_bytes().to_vec();
                 let mut s_bytes = signature.1.to_bytes().to_vec();
                 r_bytes.append(&mut s_bytes);
@@ -79,4 +76,92 @@ pub extern "system" fn Java_io_iohk_sidechains_jubjub_JubjubJniBindings_createAT
         byte_array_from_java(env, data).and_then(|native| byte_array_to_java(env, &native));
 
     return_or_throw(env, result)
+}
+
+/// Private functions
+
+fn byte_array_from_java(env: JNIEnv, data: jbyteArray) -> Result<Vec<u8>, String> {
+    return env
+        .convert_byte_array(data)
+        .map_err(|error| format!("{}", error));
+}
+
+fn byte_array_to_java(env: JNIEnv, data: &Vec<u8>) -> Result<jbyteArray, String> {
+    return env
+        .byte_array_from_slice(data)
+        .map_err(|error| format!("{}", error));
+}
+
+// Converts 64 bytes array into jubjub private key bytes
+fn java_byte_array_to_jubjub_private_key(
+    env: JNIEnv,
+    bytes: jbyteArray,
+) -> Result<PrivateKey, String> {
+    byte_array_from_java(env, bytes).and_then(|bytes| {
+        bytes
+            .as_slice()
+            .try_into()
+            .map_err(|_| String::from("private key length is invalid"))
+            .map(|s| PrivateKey::from_bytes_wide(s))
+    })
+}
+
+// Converts 32 bytes array into jubjub private key bytes
+fn java_byte_array_to_jubjub_public_key(
+    env: JNIEnv,
+    bytes: jbyteArray,
+) -> Result<PublicKey, String> {
+    byte_array_from_java(env, bytes).and_then(|bytes| {
+        bytes
+            .as_slice()
+            .try_into()
+            .map_err(|_| String::from("public key length is invalid"))
+            .and_then(|s| {
+                let ct_opt = PublicKey::from_bytes(s);
+                if ct_opt.is_some().unwrap_u8() == 1 {
+                    Ok(ct_opt.unwrap())
+                } else {
+                    Err(String::from("public key is invalid"))
+                }
+            })
+    })
+}
+
+// Converts 64 bytes array into jubjub EdDsaSignature
+fn java_byte_array_to_jubjub_eddsa_sig(
+    env: JNIEnv,
+    bytes: jbyteArray,
+) -> Result<EdDsaSignature, String> {
+    byte_array_from_java(env, bytes).and_then(|bytes| {
+        let slice: Result<[u8; 64], String> = bytes
+            .as_slice()
+            .try_into()
+            .map_err(|_| String::from("EdDSA signature length is invalid"));
+        slice.and_then(|s| {
+            let mut r_bytes: [u8; 32] = [0u8; 32];
+            r_bytes.copy_from_slice(&s[0..32]);
+            let r_ct_opt = SubgroupPoint::from_bytes(&r_bytes);
+
+            let mut s_bytes: [u8; 32] = [0u8; 32];
+            s_bytes.copy_from_slice(&s[32..64]);
+            let s_ct_opt = Scalar::from_bytes(&s_bytes);
+
+            if (s_ct_opt.is_some().unwrap_u8() == 1) && (r_ct_opt.is_some().unwrap_u8() == 1) {
+                Ok((r_ct_opt.unwrap(), s_ct_opt.unwrap()))
+            } else {
+                Err(String::from("EdDSA signature is invalid"))
+            }
+        })
+    })
+}
+
+fn return_or_throw<T, U: std::fmt::Display>(env: JNIEnv, maybe_value: Result<T, U>) -> T {
+    match maybe_value {
+        Ok(value) => value,
+        Err(error) => {
+            let msg = format!("{}", error);
+            let _ = env.throw_new("java/lang/Exception", msg);
+            return unsafe { ::std::mem::zeroed() };
+        }
+    }
 }
